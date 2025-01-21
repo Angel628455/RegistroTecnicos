59f1c2deaae3a97aafa6305b891ca0bae910aed1
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-01-21 15:07:31 -0500

    Haciendo migracion

---
+++ RegistroTecnico/Models/Tecnicos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RegistroTecnico.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+
+        public string Nombre { get; internal set; }
+
+        public decimal SueldoHora { get; set; }
+    }
+}
---
+++ RegistroTecnico/Services/TecnicoServices.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnico.DAL;
+using RegistroTecnico.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnico.Services;
+public class TecnicoService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<bool> Existe(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Tecnico.AnyAsync(t => t.TecnicoId == id);
+    }
+
+    private async Task<bool> Insertar(Tecnicos tecnico)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Tecnico.Add(tecnico);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar(Tecnicos tecnico)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Tecnico.Update(tecnico);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Guardar(Tecnicos tecnico)
+    {
+        if (!await Existe(tecnico.TecnicoId))
+            return await Insertar(tecnico);
+        else
+            return await Modificar(tecnico);
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        var Tecnicos = await contexto.Tecnico
+            .Where(t => t.TecnicoId == id).ExecuteDeleteAsync();
+        return Tecnicos > 0;
+    }
+    public async Task<Tecnicos?> Buscar(int id)
+    {
+        // Crear el contexto con using para asegurar su disposición adecuada.
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+
+        // Realizar la consulta y devolver el resultado.
+        return await contexto.Tecnico.AsNoTracking()
+            .FirstOrDefaultAsync(t => t.TecnicoId == id);
+    }
+
+    public async Task<Tecnicos?> BuscarNombres(string nombre)
+    {
+        // Crear el contexto con using para asegurar su disposición adecuada.
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+
+        // Realizar la consulta y devolver el resultado.
+        return await contexto.Tecnico.AsNoTracking()
+            .FirstOrDefaultAsync(t => t.Nombre == nombre);
+    }
+
+
+    
+
+    public async Task<List<Tecnicos>> Listar(Expression<Func<Tecnicos, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Tecnico.AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+    }
+}
