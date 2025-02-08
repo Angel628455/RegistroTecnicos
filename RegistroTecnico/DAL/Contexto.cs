1467d2052d2a36b09a6c6d0ac412a801c989c27b
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-02-08 15:52:11 -0500

    Se agrego     public Contexto(DbContextOptions<Contexto> options) : base(options) { }     public DbSet<Tecnicos> Tecnico { get; set; }     public DbSet<Clientes> Clientes { get; set; }     public DbSet<Ciudades> Ciudades { get; set; }     public DbSet<Tickets> Tickets { get; internal set; } }

--- RegistroTecnico/DAL/Contexto.cs
+++ RegistroTecnico/DAL/Contexto.cs
@@ -1,11 +1,13 @@
 ﻿using RegistroTecnico.Models;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace RegistroTecnico.DAL;
 
 public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
-    public DbSet<Tecnicos> Tecnico {  get; set; }
+    public DbSet<Tecnicos> Tecnico { get; set; }
+    public DbSet<Clientes> Clientes { get; set; }
+    public DbSet<Ciudades> Ciudades { get; set; }
+    public DbSet<Tickets> Tickets { get; internal set; }
 }
